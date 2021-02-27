cf726b90205e3726ae9c6eef7b7c74a54130c821
EHuiMartinez <69170347+EHuiMartinez@users.noreply.github.com>
2021-02-26 23:29:59 -0500

    update query

--- Database/query.sql
+++ Database/query.sql
@@ -9,3 +9,17 @@ select * from labs;
 select * from labs_blood_gas;
 
 select * from demographics;
+
+
+select * from demographics 
+join apache_covariate
+    on demographics.encounter_id = apache_covariate.encounter_id
+join vitals
+    on demographics.encounter_id = vitals.encounter_id
+join labs
+    on demographics.encounter_id = labs.encounter_id
+join labs_blood_gas
+    on demographics.encounter_id = labs_blood_gas.encounter_id
+join apache_comorbidity
+    on demographics.encounter_id = apache_comorbidity.encounter_id;
+
